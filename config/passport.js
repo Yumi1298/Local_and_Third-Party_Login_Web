153e4e073ecf383cf43d610d704dd6ceec80c389
Yumi1298 <nhb12980712@gmail.com>
2024-10-21 14:31:33 +0800

    簡易檢查此使用者是否已註冊

--- config/passport.js
+++ config/passport.js
@@ -1,5 +1,6 @@
 const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth20");
+const User = require("../models/user-model");
 
 passport.use(
   new GoogleStrategy(
@@ -9,8 +10,16 @@ passport.use(
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
       callbackURL: "/auth/google/redirect",
     },
-    (accessToken, refreshToken, profile, done) => {
+    async (accessToken, refreshToken, profile, done) => {
+      console.log("進入google strategy 的區域");
       console.log(profile);
+      console.log("====================");
+      let foundUser = await User.findOne({ googleID: profile.id }).exec();
+      if (foundUser) {
+        console.log("使用者已經註冊過了，無須存入資料庫內");
+      } else {
+        console.log("偵測到新用戶。需將資料存入資料庫");
+      }
     }
   )
 );
